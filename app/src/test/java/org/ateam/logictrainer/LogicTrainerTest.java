1b1d48494cad32668c27fa83c852fcade6a796b4
ndrprssnr <60465452+ndrprssnr@users.noreply.github.com>
2021-01-17 21:50:50 +0100

    Move color chooser (#2)
    * test added
    
    * color chooser included in main layout
    
    * color chooser panel wired up
    
    - color chooser dialog replaced by on-board panel
    - tests added
    
    * gradle updated, screenshots added
    
    * gradle wrapper updated
    
    * gradle wrapper jar added
    
    Co-authored-by: Andre <a>

--- app/src/test/java/org/ateam/logictrainer/LogicTrainerTest.java
+++ app/src/test/java/org/ateam/logictrainer/LogicTrainerTest.java
@@ -22,4 +22,26 @@ public class LogicTrainerTest {
 		LogicTrainer trainer = new LogicTrainer(new Options());
 		assertTrue(trainer.isNotGameOver());
 	}
+
+	@Test
+	public void testInitialGameCannotCheck() {
+		LogicTrainer trainer = new LogicTrainer(new Options());
+		assertFalse(trainer.canCheck());
+	}
+
+	@Test
+	public void testInitialGameMakeStep() {
+		LogicTrainer trainer = new LogicTrainer(new Options());
+		LogicTrainer.Result result = trainer.makeStep();
+		assertNotNull(result);
+		assertEquals(0, result.black);
+		assertEquals(0, result.white);
+	}
+
+	@Test
+	public void testInitialGameHasEmptyCodebreakerPanel() {
+		LogicTrainer trainer = new LogicTrainer(new Options());
+		assertEquals(0, trainer.getCodebreakerPanels().size());
+	}
+
 }
